ad01d1814081785a1509fc7587652e7df0cdd4c0
kymkyj <ymyj33@naver.com>
2022-03-26 21:59:43 +0900

    [feat] 재귀 이진수 출력

--- src/inflearn/RecursiveBinary.java
+++ src/inflearn/RecursiveBinary.java
@@ -2,7 +2,12 @@ package inflearn;
 
 public class RecursiveBinary {
     public void DFS(int n){
-
+        if(n == 0) return;
+        else{
+            DFS(n / 2);
+            // 스택프레임으로 인해 아래에 출력해야 함
+            System.out.print(n%2+ " ");
+        }
     }
 
     public static void main(String[] args) {
